de20557b5358cb2d68394f5c62e991efa279eb7a
Saurav109677 <34793927+Saurav109677@users.noreply.github.com>
2022-04-08 18:31:11 +0530

    added invitation for ramnavmi

---
+++ invitation.js
@@ -0,0 +1,30 @@
+ const invitation ="🎉🎊🎉🎊🎉🎊🎊✨🎊\n"+
+"☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️\n\n"+
+
+"*Haldia VOICE*\n"+
+"celebrating *RAMNVAMI*\n\n"+
+"*the Holy Appearance of Lord Rama*"
+
+"*Teachings of Ramayana*🎉🎊\n\n"+
+
+"*Events:*\n"+
+"🎤Ram Katha\n"+
+"🎶Music & Dance\n"+
+"😋Delicious Feast\n\n"+
+
+
+"*Speaker :* 🎙️\n"+
+"*HG Kumar Lila Das* ❉\n"+
+"IIT BHU\n"+
+"Motivational Speaker\n\n"+
+
+"*Venue -* VOICE (offline)\n"+  
+"*Date -* Apr 09 & 10, 2022 (Sat & Sun)🗓️\n"+
+"*Time -* 5:30 PM onwards ⏰\n\n"+
+
+"® *CONTACT US FOR REGISTRATION* \n\n"+
+
+"Your ever well-wisher\n"+
+"*VOICE* 🌼\n";
+
+module.exports = invitation
